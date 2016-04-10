dad44cfc5f9b026ed505ca17bde2ec55692c2314
Anton Rylov <anton.rylov@propeople.com.ua>
2016-04-10 18:37:16 +0300

    Create header and background video container

--- js/init.js
+++ js/init.js
@@ -1,3 +1,10 @@
 (function($) {
+  'use strict';
 
+  $('.video').vide('video/video2', {
+    'mp4': 'video/video2',
+    'webm': 'video/video2',
+    'ogv': 'video/video2',
+    'poster': 'video/video2'
+  });
 })(jQuery);
